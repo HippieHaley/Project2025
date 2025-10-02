fd07b6cc40e29f6efc7ee271133c603b4cd3341a
HippieHaley <haley.m.patino@gmail.com>
2025-10-02 22:43:06 +0000

    broken

--- catalog.js
+++ catalog.js
@@ -35,6 +35,11 @@ const procedures = [
     visitType: "new",
     keywords: ["comprehensive-new", "new pt", "99204"]
   },
+{
+  break: true,
+  category: "visits" // still needs to know what section to drop into
+}
+,
   {
     name: "Est Pt Minimal (RN)",
     cpt: "99211",
